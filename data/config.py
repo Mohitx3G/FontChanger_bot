c45ebc149544733af81f921ce93ef1220cc4f907
Mohitx3G <95665448+Mohitx3G@users.noreply.github.com>
2024-06-08 12:08:50 +0530

    Update config.py

--- data/config.py
+++ data/config.py
@@ -5,10 +5,10 @@ env = Env()
 env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot token
+BOT_TOKEN = env.str("BOT_TOKEN", "7414641943:AAGoiVcEpbFdC2o-dQKq95TY8XKvLffuAKs")  # Bot token
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 # IP = env.str("ip")  # Hosting ip manzili
 BACKEND_URL = env.str("BACKEND_URL")  # BACKEND_URL manzili
 ADMIN_TOKEN = env.str("ADMIN_TOKEN")  # Admin tokeni
 
-CHANNELS = ['-1001437339488']  # channels
+CHANNELS = ['-1001997121709']  # channels
